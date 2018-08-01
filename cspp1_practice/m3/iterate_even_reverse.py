64d015b627f62a355cfe40b16e3d02056f6c3006
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-01 20:40:32 +0530

    iteerate and sum while

---
+++ cspp1_practice/m3/iterate_even_reverse.py
@@ -0,0 +1,9 @@
+'''
+@author :kpkishankrishna
+iterate even reverse
+'''
+n = 10
+print("Hello!")
+while n >= 2:
+    print(n)
+    n -= 2
---
+++ cspp1_practice/m3/sum_1_end.py
@@ -0,0 +1,11 @@
+'''
+@author :kpkishankrishna
+iterate even reverse
+'''
+n = 1
+a = int(input())
+sum=0
+while n <= a:
+    sum = sum + n
+    n += 1
+print(sum)
