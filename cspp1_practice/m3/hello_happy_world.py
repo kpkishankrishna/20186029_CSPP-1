20623e68e4b0fb05cc1f147abaf1c8668aeadeee
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-01 17:58:40 +0530

    My second programme

---
+++ cspp1_practice/m3/hello_happy_world.py
@@ -0,0 +1,9 @@
+'''
+@author :kpkishankrishna
+Write a piece of Python code that prints out the string
+'hello world' if the value of an integer variable,
+happy, is strictly greater than 2.
+'''
+HAPPY = int(input())
+if HAPPY > 2:
+    print("hello world")
