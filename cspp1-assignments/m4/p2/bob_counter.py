fe825ad895b0803636348d42b9b495e076c2cc98
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-02 19:43:11 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

---
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -0,0 +1,22 @@
+'''
+author@ : kpkishankrishna
+Assume s is a string of lower case characters.
+Write a program that prints the number of times the string 'bob' occurs in s.
+For example, if s = 'azcbobobegghakl', then your program should print
+Number of times bob occurs is: 2
+'''
+
+def main():
+    '''
+    the input string is in s
+    remove pass and start your code here
+    '''
+    input_s = input()
+    num_bob = 0
+    value_bob = "bob"
+    for i in range(len(input_s)-2):
+        if input_s[i] + input_s[i+1] + input_s[i+2] == value_bob:
+            num_bob += 1
+    print(num_bob)
+if __name__ == "__main__":
+    main()
