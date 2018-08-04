37b7bfa4309d7d757a03f73480d197969d46ba12
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-04 11:32:30 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -9,14 +9,14 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
-    int_a = int_input
-    int_n = 0
+    int_a = abs(int_input)
+        int_n = 0
     int_pro = 1
     while int_a > 0:
         int_n = int_a%10
         int_pro = int_pro*int_n
         int_a = int_a//10
-    print(int_pro)
+        print(int_pro)
 
 if __name__ == "__main__":
     main()
