3011a8842393e2660478285f62304f62cc021255
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-04 11:40:09 +0530

    " -> 1 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignments/m6/p2/special_char.py
+++ cspp1-assignments/m6/p2/special_char.py
@@ -12,7 +12,7 @@ def main():
     str_in2 = ""
     for i in str_input:
         if i in "!@#$%^&*()_+{}:":
-            print( )
+            print(" ")
         else:
             str_in2 += i
     print(str_in2)
