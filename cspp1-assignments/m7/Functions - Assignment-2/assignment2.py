03ad98ad283863e3467fc3e5d85420721242a361
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-07 00:10:40 +0530

    " -> 10 of 10 passed. pylint: 6.67/10 "

--- cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
+++ cspp1-assignments/m7/Functions - Assignment-2/assignment2.py
@@ -2,8 +2,10 @@
 Author@ : kpkishankrishna
 # Assignment-2 - Paying Debt off in a Year
 
-# Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months.
-# By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be
+# Now write a program that calculates the minimum fixed monthly payment needed in order
+pay off a credit card balance within 12 months.
+# By a fixed monthly payment, we mean a single number which does not change each month,
+ but instead is a constant amount that will be
 # paid each month.
 
 # In this problem, we will not be dealing with a minimum monthly payment rate.
@@ -12,16 +14,21 @@ Author@ : kpkishankrishna
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
 
-# The program should print out one line: the lowest monthly payment that will pay off all debt in under 1 year, for example:
+# The program should print out one line: the lowest monthly payment that
+ will pay off all debt in under 1 year, for example:
 # Lowest Payment: 180
 
-# Assume that the interest is compounded monthly according to the balance at the end of the month (after the payment for that month is
+# Assume that the interest is compounded monthly according to the balance at the
+ end of the month (after the payment for that month is
 # made).
-# The monthly payment must be a multiple of $10 and is the same for all months. Notice that it is possible for the balance to become
-# negative using this payment scheme, which is okay. A summary of the required math is found below:
+# The monthly payment must be a multiple of $10 and is the same for all months.
+ Notice that it is possible for the balance to become
+# negative using this payment scheme, which is okay. A summary of the required math
+ is found below:
 # Monthly interest rate = (Annual interest rate) / 12.0
 # Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x
+ Monthly unpaid balance)
 '''
 
 def payingDebtOffInAYear(balance_kp, annualInterestRate, set_min):
@@ -38,9 +45,9 @@ def payingDebtOffInAYear(balance_kp, annualInterestRate, set_min):
         month += 1
         monthly_unpaid = balance_kp - minimum_fixed
         balance_kp = monthly_unpaid + (monthly_interest*monthly_unpaid)
-        if monthly_unpaid<=0 and month == 12:
+        if monthly_unpaid <=0 and month == 12:
             return minimum_fixed
-        if month == 12 and monthly_unpaid >0:
+        if month == 12 and monthly_unpaid > 0:
             month = 0
             minimum_fixed += 10
             balance_kp = set_min
