9b2ac737aee6420c580be42de185d303ff844312
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-08 17:26:25 +0530

    " -> 0 of 9 passed. pylint: 0/0 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -18,9 +18,11 @@ def is_word_guessed(secret_word, letters_guessed):
     for i in secret_word:
         for j in letters_guessed:
             if i == j:
-                return True
-            else:
-                return False
+               count = count+1
+    if count = len(secret_word):
+        return True
+    else:
+        return False
 
 def main():
     '''
