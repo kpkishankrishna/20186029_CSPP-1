cbb7fa2c3bd03ebb1a2faf58dfd2fe8657031d69
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-08 18:16:38 +0530

    " -> 0 of 9 passed. pylint: 0/0 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -21,21 +21,21 @@ def is_word_guessed(secret_word, letters_guessed):
             return False
     return True
 
-    def main():
-    '''
-    Main function for the program
-    '''
-    user_input = input()
-    if user_input:
-        data = user_input.split()
-        secret_word = data[0]
-    else:
-        data = []
-        secret_word = ""
-    list1 = []
-    for j in range(1, len(data)):
-        list1.append(data[j][0])
-    print(is_word_guessed(secret_word, list1))
+def main():
+'''
+Main function for the program
+'''
+user_input = input()
+if user_input:
+    data = user_input.split()
+    secret_word = data[0]
+else:
+    data = []
+    secret_word = ""
+list1 = []
+for j in range(1, len(data)):
+    list1.append(data[j][0])
+print(is_word_guessed(secret_word, list1))
 
 if __name__ == "__main__":
     main()
