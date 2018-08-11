d341969b750a3da24d8ffa09fa3365c512de75de
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-11 12:40:57 +0530

    " -> 0 of 2 passed. pylint: 9.60/10 "

--- cspp1-assignments/m12/p2/social_network.py
+++ cspp1-assignments/m12/p2/social_network.py
@@ -14,7 +14,7 @@ def follow(network, arg1, arg2):
         update the network dictionary and return it
     '''
     # remove the pass below and start writing your code
-    network[arg1].append(arg2)
+    network[arg1].extend(arg2)
     return network
 
 
