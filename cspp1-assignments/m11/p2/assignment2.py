f2295a748afcdd3fe8cfea6f58de06636556b0a9
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-12 21:59:23 +0530

    " -> 6 of 6 passed. pylint: -8.12/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -19,6 +19,10 @@ def updateHand(hand, word):
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
+    for i in word:
+        if i in hand:
+            hand[i] -= 1
+    return hand
     
 
 def main():
