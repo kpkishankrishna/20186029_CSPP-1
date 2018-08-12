b355cbbe8f138a9432f761e1b739229f5f98623e
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-12 22:00:37 +0530

    " -> 6 of 6 passed. pylint: -0.62/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -23,19 +23,22 @@ def updateHand(hand, word):
         if i in hand:
             hand[i] -= 1
     return hand
-    
+
 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	data1=input()
-	print(updateHand(adict,data1))
-		
+    '''
+    main function
+    '''
+    n=input()
+    adict={}
+    for i in range(int(n)):
+        data=input()
+        l=data.split()
+        adict[l[0]]=int(l[1])
+    data1=input()
+    print(updateHand(adict,data1))
+
 
 
 if __name__== "__main__":
-	main()
+    main()
