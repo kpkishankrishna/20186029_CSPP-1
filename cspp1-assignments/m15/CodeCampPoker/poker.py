5ef5fd1c0fb048ededf7af3b075bf64962513714
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-16 14:13:06 +0530

    " -> 14 of 19 passed. pylint: 5.83/10 "

--- cspp1-assignments/m15/CodeCampPoker/poker.py
+++ cspp1-assignments/m15/CodeCampPoker/poker.py
@@ -74,25 +74,25 @@ def five_of_kind(hand):
 
 
 def four_of_kind(hand):
-    return len(set(face_values(hand))) == 2
-    '''
-    a= face_values(hand)
-    a.sort()
-    b=[]
-    c=[]
-    for i in a:
-        if a[0] == i:
-            b.append(i)
+    while len(set(face_values(hand))) == 2:
+            
+        a= face_values(hand)
+        a.sort()
+        b=[]
+        c=[]
+        for i in a:
+            if a[0] == i:
+                b.append(i)
+            else:
+                c.append(i)
+        if len(b) == 4 and len(c) == 1:
+            return True
+        elif len(b) == 1 and len(c) == 4:
+            return True
         else:
-            c.append(i)
-    if len(b) == 4 and len(c) == 1:
-        return True
-    elif len(b) == 1 and len(c) == 4:
-        return True
-    else:
-        return False 
+            return False 
 
-    '''  
+     
 
 def three_of_kind(hand):
     return len(set(face_values(hand))) == 3
