d33921730d31ded4a5d923ad825eb0cdf5004263
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-16 10:52:08 +0530

    " -> 0 of 19 passed. pylint: 0/0 "

--- cspp1_practice/m3/rough.py
+++ cspp1_practice/m3/rough.py
@@ -1,1 +1,9 @@
-p
+'''
+dict_face = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+def face_values(hand):
+    face_values = []
+    for i in hand:
+        face_values.append(dict_face[i[0]])
+    return face_values
+ '''
+print(len(set([1, 1, 1, 1, 2])))
