6bcdf9f3a97fcc27271878d8a3ae8789ef4c6217
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-17 21:03:53 +0530

    " -> 0 of 3 passed. pylint: 5.58/10 "

--- cspp1_practice/m3/learn dict.py
+++ cspp1_practice/m3/learn dict.py
@@ -1,20 +1,83 @@
-import string
-s = string.ascii_lowercase[:]
-print(s)
-d = {}
-for i in s:
-	if i not in d:
-		d[i] = 1
-	else:
-		d[i] += 1
-print(d)
-print("...................")
-d_1 = {}
-for i in d:
-	if d[i] not in d_1:
-		d_1[d[i]] = []
-		d_1[d[i]].append(i)
-	else:
-		d_1[d[i]].append(i)
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
+import re
+import math
+def combine_dictionaries(dictionary_one, dictionary_two):
+    '''
+    two dictionaries are combined
+    '''
+    dictionary = {}
+    for word in dictionary_one, dictionary_two:
+        if word in dictionary_two:
+            dictionary[word] = [dictionary_one[word], dictionary_two[word]]
 
-print(d_1)
+    for word in dictionary_one:
+        if word not in dictionary:
+            dictionary[word] = [dictionary_one[word], 0]
+    for word in dictionary_two:
+        if word not in dictionary:
+            dictionary[word] = [0, dictionary_two[word]]
+    return dictionary
+
+def calculate_similarity(dictionary_values):
+    '''
+    calculating frequency
+    '''
+    numerator = sum([k[0] * k[1] for k in dictionary_values()])
+    d1_a = math.sqrt(sum([k[0] ** 2 for k in dictionary_values()]))
+    d2_a = math.sqrt(sum([k[1] ** 2 for k in dictionary_values()]))
+    return numerator/(d1_a*d2_a)
+def create_dictionary(words_list):
+    '''
+    returns dictionary, takes input as word list
+    '''
+    dictionary = {}
+    stopwords = load_stopwords("stopwords.txt")
+    for word in words_list:
+        word = word.strip()
+        if word not in stopwords and words_list.len(word) > 0:
+            if word not in dictionary:
+                dictionary[word] = 1
+            else:
+                dictionary[word] += 1
+    return dictionary
+
+def clean_given_text(text_input):
+    '''
+    takes string and return list
+    '''
+    words = text_input.lower().strip().replace('\'', '')
+    regex = re.compile('[^a-z]')
+    words = regex.sub(" ", words).split(" ")
+    return words
+def similarity(text_1, text_2):
+    '''
+    Compute the document distance as given in the PDF
+    '''
+    dictionary_one = clean_given_text(text_1)
+    dictionary_two = clean_given_text(text_2)
+    dictionary = combine_dictionaries(dictionary_one, dictionary_two)
+    return calculate_similarity(dictionary)
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename_n:
+        for line in filename_n:
+            stopwords[line.strip()] = 0
+    return stopwords
+
+def main():
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input()
+    input2 = input()
+
+    print(similarity(input1, input2))
+
+if __name__ == '__main__':
+    main()
