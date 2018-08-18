caa2840275a5dd7221147f8702ed6827e59af08e
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-18 12:09:45 +0530

    " -> 0 of 1 passed. pylint: 8.29/10 "

--- cspp1_practice/m3/rough.py
+++ cspp1_practice/m3/rough.py
@@ -1,15 +1,46 @@
-docs = "my name is my name is krishna prasad krishna prasad"
-import re
-words = docs.lower().strip().replace('\'', '')
-regex = re.compile('[^a-z]')
-words = regex.sub(" ", words).split(" ")
-print(words)
+# import re
+def load_stopwords(filename):
+    stopwords = {}
+    with open(filename, 'r') as f_stopwords:
+        for line in f_stopwords:
+            stopwords[line.strip()] = 0
+    return stopwords
+def word_list(string):
+    regex = re.compile('[^a-z]')
+    return [regex.sub("", w.strip()) for w in string.lower().split(" ")]
+def build_search_index(docs):
+    dict_1 = {}
+    STOP_WORD = load_stopwords("stopwords.txt")
+    for index, line in enumerate(docs):
+        LIST_ = remove_stopwords(word_list(line), STOP_WORD)
+        for word in set(LIST_):
+            if word in dict_1:
+                dict_1[word].append((index, LIST_.count(word)))
+            else:
+                dict_1[word] = [(index, LIST_.count(word))]
+    return dict_1
 
+def remove_stopwords(word, STOP_WORD):
+    LIST_1 = word
+    for w_1 in word:
+        if w_1 in STOP_WORD:
+            LIST_1.remove(w_1)
+    return LIST_1
+def print_search_index(index):
+keys = sorted(index.keys())
+for key in keys:
+    print(key, " - ", index[key])
+def main():
+    '''
+        main function
+    '''
+    documents = []
+    lines = int(input())
+    for i in range(lines):
+        documents.append(input())
+        i += 1
 
-big_dict = {}
-for i in words:
-    if i not in big_dict:
-        big_dict[i] = 1
-    else:
-        big_dict[i] += 1
-print(big_dict)
+    print_search_index(build_search_index(documents))
+
+if __name__ == '__main__':
+    main()
