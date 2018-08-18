b48110369bb4a47c90bd87998d162307893988d0
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-18 11:01:43 +0530

    " -> 0 of 1 passed. pylint: 2.89/10 "

--- cspp1_practice/m3/learn dict.py
+++ cspp1_practice/m3/learn dict.py
@@ -1,83 +1,36 @@
-'''
-    Document Distance - A detailed description is given in the PDF
-'''
-import re
-import math
-def combine_dictionaries(dictionary_one, dictionary_two):
-    '''
-    two dictionaries are combined
-    '''
-    dictionary = {}
-    for word in dictionary_one, dictionary_two:
-        if word in dictionary_two:
-            dictionary[word] = [dictionary_one[word], dictionary_two[word]]
+import string
+file1 = "Krisha kriSha prasAd _ 1 is .a ?good guy"
+file2 = "gangadhar gangadHar sIngh _ 1 iS .a ?good guy"
+words = file1.lower().split(' ')
+print(words)
+word2 =file2.lower().split(' ')
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in words]
+print(stripped)
+stripped2 = [w.translate(table) for w in word2]
+dict_1 = {}
+dict_2 = {}
+lis_1= []
+cnt = 1
+cnt2 = 1
+for i in stripped:
+    if i not in dict_1:
+        dict_1[i] = 1
+    else:
+        dict_1[i] += 1
+print(dict_1)
+for i in stripped2:
+    if i not in dict_2:
+        dict_2[i] = cnt
+    else:
+        dict_2[i] = cnt+1
+print(dict_2)
+for i in dict_1:
+    if i in '1234567890':
+        i += ' '
+for i in dict_2:
+    if i in '1234567890':
+        i += ' '
 
-    for word in dictionary_one:
-        if word not in dictionary:
-            dictionary[word] = [dictionary_one[word], 0]
-    for word in dictionary_two:
-        if word not in dictionary:
-            dictionary[word] = [0, dictionary_two[word]]
-    return dictionary
-
-def calculate_similarity(dictionary_values):
-    '''
-    calculating frequency
-    '''
-    numerator = sum([k[0] * k[1] for k in dictionary_values()])
-    d1_a = math.sqrt(sum([k[0] ** 2 for k in dictionary_values()]))
-    d2_a = math.sqrt(sum([k[1] ** 2 for k in dictionary_values()]))
-    return numerator/(d1_a*d2_a)
-def create_dictionary(words_list):
-    '''
-    returns dictionary, takes input as word list
-    '''
-    dictionary = {}
-    stopwords = load_stopwords("stopwords.txt")
-    for word in words_list:
-        word = word.strip()
-        if word not in stopwords and words_list.len(word) > 0:
-            if word not in dictionary:
-                dictionary[word] = 1
-            else:
-                dictionary[word] += 1
-    return dictionary
-
-def clean_given_text(text_input):
-    '''
-    takes string and return list
-    '''
-    words = text_input.lower().strip().replace('\'', '')
-    regex = re.compile('[^a-z]')
-    words = regex.sub(" ", words).split(" ")
-    return words
-def similarity(text_1, text_2):
-    '''
-    Compute the document distance as given in the PDF
-    '''
-    dictionary_one = clean_given_text(text_1)
-    dictionary_two = clean_given_text(text_2)
-    dictionary = combine_dictionaries(dictionary_one, dictionary_two)
-    return calculate_similarity(dictionary)
-
-def load_stopwords(filename):
-    '''
-        loads stop words from a file and returns a dictionary
-    '''
-    stopwords = {}
-    with open(filename, 'r') as filename_n:
-        for line in filename_n:
-            stopwords[line.strip()] = 0
-    return stopwords
-
-def main():
-    '''
-        take two inputs and call the similarity function
-    '''
-    input1 = input()
-    input2 = input()
-
-    print(similarity(input1, input2))
-
-if __name__ == '__main__':
-    main()
+print(dict_1)
+print(dict_2)
--- cspp1_practice/m3/rough.py
+++ cspp1_practice/m3/rough.py
@@ -1,36 +1,48 @@
-import string
-file1 = "Krisha kriSha prasAd _ 1 is .a ?good guy"
-file2 = "gangadhar gangadHar sIngh _ 1 iS .a ?good guy"
-words = file1.lower().split(' ')
-print(words)
-word2 =file2.lower().split(' ')
-table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in words]
-print(stripped)
-stripped2 = [w.translate(table) for w in word2]
-dict_1 = {}
-dict_2 = {}
-lis_1= []
-cnt = 1
-cnt2 = 1
-for i in stripped:
-    if i not in dict_1:
-        dict_1[i] = 1
-    else:
-        dict_1[i] += 1
-print(dict_1)
-for i in stripped2:
-    if i not in dict_2:
-        dict_2[i] = cnt
-    else:
-        dict_2[i] = cnt+1
-print(dict_2)
-for i in dict_1:
-    if i in '1234567890':
-        i += ' '
-for i in dict_2:
-    if i in '1234567890':
-        i += ' '
+input_1 = "My name is Krishna Prasad 1234"
+input_2 = "My name is gangadhar singh"
+import re
 
-print(dict_1)
-print(dict_2)
+# dict_3 = {}
+# for i in dict_1:
+#     if i not in dict_3:
+#         dict_3[i] = 1
+#     else:
+#         dict_3[i] += 1
+# print(dict_3)
+def clean_string(words):
+    words_1 = input_1.lower().strip().replace('\'', '')
+    regex = re.compile('[^a-z]')
+    words_1 = regex.sub(" ", words_1).split(" ")
+    print(words_1)
+def create_dic(words_l):
+    '''return dictionary and input as wordlist
+    '''
+    dict_n = {}
+    for word in words_l:
+        word = word.strip()
+        if word != '':
+            if word not in dict_n:
+                dict_n[word] = 1
+            else:
+                dict_n[word] += 1
+    return dict_n
+def combine_dictionary(dict_one, dict_two):
+    '''input: two dictionary and output: combine dictionary with value as list
+    '''
+    dict_combine = {}
+    for word in dict_one:
+        if word in dict_two:
+            if word not in dict_combine:
+                dict_combine[word] = [dict_one[word], dict_two[word]]
+
+    for word in dict_one:
+        if word not in dict_combine:
+            dict_combine[word] = [dict_one[word], 0]
+    for word in dict_two:
+        if word not in dict_combine:
+            dict_combine[word] = [0, dict_two[word]]
+    return dict_combine
+
+a = create_dic(clean_string(input_1))
+b = create_dic(clean_string(input_2))
+print(combine_dictionary(a,b))
