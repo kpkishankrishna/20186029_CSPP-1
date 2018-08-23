ecda86e8ba064d9346842b91011b3bd37e3aaada
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-23 22:22:37 +0530

    " -> 0 of 8 passed. pylint: 3.17/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -19,7 +19,7 @@ def mult_matrix(m1, m2):
     print("Error: Matrix shapes invalid for mult")
     return None
             
-    return multi_matrix
+    
                  
 
 def add_matrix(m1, m2):
@@ -37,11 +37,12 @@ def add_matrix(m1, m2):
         add_matrix = [[0 for i in range(columns)] for j in range(rows)]
         for i in range(rows):
             for j in range(columns):
-                add_matrix[i][j] = m1[i][j] + m2[i][j]  
+                add_matrix[i][j] = m1[i][j] + m2[i][j]
+        return add_matrix  
     else:
         print("Error: Matrix shapes invalid for addition")
         return None  
-    return add_matrix
+    
 
 def read_matrix():
     '''
