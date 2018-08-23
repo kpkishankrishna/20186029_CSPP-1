9c619ac6f5a532e6a56f0e50fa0b52d704826c4d
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-23 22:15:46 +0530

    " -> 0 of 8 passed. pylint: -11.43/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -33,7 +33,7 @@ def add_matrix(m1, m2):
     matrix_1 = []
     rows = len(m1)
     columns = len(m1[0])
-     if len(m_1) == len(m_2) and len(m_1[0]) == len(m_2[0]):
+    if len(m_1) == len(m_2) and len(m_1[0]) == len(m_2[0]):
         add_matrix = [[0 for i in range(columns)] for j in range(rows)]
         for i in range(rows):
             for j in range(columns):
