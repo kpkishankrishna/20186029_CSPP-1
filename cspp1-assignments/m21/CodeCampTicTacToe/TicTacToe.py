59c662bb2ba3e35d6da76a51fbf05f44151f4b30
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-24 09:51:42 +0530

    " -> 0 of 14 passed. pylint: -9.09/10 "

---
+++ cspp1-assignments/m21/CodeCampTicTacToe/TicTacToe.py
@@ -0,0 +1,21 @@
+def read_input():
+	input_1 = input().strip()
+	if len(input_1) >= 9:
+		return ("invalid input")
+
+
+
+
+
+def check_winner(game_input):
+	pass
+
+
+
+def main():
+	game_input = read_input()
+	winner = check_winner(game_input)
+
+
+if __name__ == '__main__':
+    main()
