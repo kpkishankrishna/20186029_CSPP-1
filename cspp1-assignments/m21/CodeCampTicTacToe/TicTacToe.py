6c59fe6b68a2a58acb286258b78a6defc9cb0ce9
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-24 10:10:43 +0530

    " -> 0 of 14 passed. pylint: -12.50/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/TicTacToe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/TicTacToe.py
@@ -2,7 +2,7 @@ def read_input():
 	input_1=[]
 	for i in range(3):
 		input_1.append(input().split(" "))
-	return input_1
+	print(input_1)
 
 
 
@@ -15,7 +15,6 @@ def check_winner(game_input):
 
 def main():
 	game_input = read_input()
-	print(game_input)
 	winner = check_winner(game_input)
 
 
