cc52362fc837a7b23f717cf3252b7333228b1cb3
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-24 16:46:48 +0530

    " -> 0 of 14 passed. pylint: -7.00/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/TicTacToesir.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/TicTacToesir.py
@@ -2,8 +2,11 @@
 def main():
     intac = tictac_input()
     valid_check = check_valid(intac)
-    if valid_check == True:
-        pass
+    if valid_check(intac):
+        if valid_game(intac):
+            pass
+        else:
+            print("invalid game")    
     else:
         print("invalid input")
 
@@ -20,7 +23,15 @@ def check_valid(intac):
     else:
         return True
 def valid_game(intac):
-    pass
+    for i in one_list_1:
+        if i not in "xo.":
+            return 1
+        elif one_list_1.count('x') > 5 or one_list_1.count('o') > 5:
+            return False
+        elif one_list_1.count('x') - one_list_1.count('o') > 1 or one_list_1.count('o') - one_list_1.count('x') >1:
+            return False
+        else:
+            return True
 
 
 
