6210308122b7756074d75dc5ffd4e5a1849670b3
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-24 16:19:21 +0530

    " -> 3 of 14 passed. pylint: 2.46/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/TicTacToe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/TicTacToe.py
@@ -14,7 +14,7 @@ def one_list(game_input):
 def valid_game_check(one_list_1):
     for i in one_list_1:
         if i not in "xo.":
-            return "1"
+            return 1
         elif one_list_1.count('x') > 5 or one_list_1.count('o') > 5:
             return False
         elif one_list_1.count('x') - one_list_1.count('o') > 1 or one_list_1.count('o') - one_list_1.count('x') >1:
@@ -84,7 +84,7 @@ def check_winner(game_input, one_list_1):
 def main():
     game_input = read_input()
     one_list_1 = one_list(game_input)
-    if valid_game_check(one_list_1) == "1":
+    if valid_game_check(one_list_1) == 1:
         print("invalid input")
     elif valid_game_check(one_list_1) == False:
         print("invalid game")
