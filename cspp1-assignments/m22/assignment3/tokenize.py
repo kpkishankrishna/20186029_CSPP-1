ad26124b2d65a94ec484e67d9a4f2733f7d48ee5
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-25 12:17:57 +0530

    " -> 1 of 4 passed. pylint: 4.67/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -8,17 +8,14 @@ def tokenize(string):
     creating a dictionary with frequency
     '''
     
-    dictionary_final={}
+    dictionary_final = {}
     for element_in_dic in string:
         if element_in_dic not in dictionary_final:
-            dictionary_final[element_in_dic]=1
+            dictionary_final[element_in_dic] = 1
         else:
-            dictionary_final[element_in_dic]+=1
+            dictionary_final[element_in_dic] += a1
     return dictionary_final
 
-    
-
-            
 def main():
     '''
     main function
@@ -32,11 +29,9 @@ def main():
     string = []
     for i in range(no_of_lines):
       string.extend(input().strip().split(" "))
-    
-
     print(tokenize(string))
 
-    
+
 
 if __name__ == '__main__':
     main()
