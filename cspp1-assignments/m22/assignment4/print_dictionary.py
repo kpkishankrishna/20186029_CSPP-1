f17cf57036bec7129d2cde2931df00bab1d389ca
kpkishankrishna <kpkishankrishna@msitprogram.net>
2018-08-25 12:57:59 +0530

    " -> 3 of 3 passed. pylint: 8.82/10 "

--- cspp1-assignments/m22/assignment4/print_dictionary.py
+++ cspp1-assignments/m22/assignment4/print_dictionary.py
@@ -1,23 +1,30 @@
 '''
+author@: kpkishankrishna
 Write a function to print a dictionary with the keys in sorted order along with the
 frequency of each word. Format of the printing should be one key per line and separate
 the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-    c=[]
+    '''
+    dictionary into list
+    '''
+    list_dict=[]
     for i in dictionary:
         if dictionary[i] == 2:
             i = i+(" - 2")
-            c.append(i)
+            list_dict.append(i)
         else:
             i = i+(" - 1")
-            c.append(i)
-    c.sort()
-    return c
-    
+            list_dict.append(i)
+    list_dict.sort()
+    return list_dict
+
 
 def main():
+    '''
+    main function
+    '''
     dictionary = eval(input())
     list_dict = print_dictionary(dictionary)
     for i in list_dict:
